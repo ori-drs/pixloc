2dd402ee12c90aebd91a95bafa515bb821cf3cae
Frank Fu <fulkast@gmail.com>
2022-01-14 18:01:20 +0000

    Add spherical projector, cropping and scaling

--- pixloc/pixlib/geometry/wrappers.py
+++ pixloc/pixlib/geometry/wrappers.py
@@ -9,6 +9,8 @@ import math
 from typing import Union, Tuple, List, Dict, NamedTuple
 import torch
 import numpy as np
+import os
+import cv2
 
 from .optimization import skew_symmetric, so3exp_map
 from .utils import undistort_points, J_undistort_points
@@ -155,7 +157,7 @@ class Pose(TensorWrapper):
     def R(self) -> torch.Tensor:
         '''Underlying rotation matrix with shape (..., 3, 3).'''
         rvec = self._data[..., :9]
-        return rvec.reshape(rvec.shape[:-1]+(3, 3))
+        return rvec.reshape(rvec.shape[:-1] + (3, 3))
 
     @property
     def t(self) -> torch.Tensor:
@@ -251,7 +253,7 @@ class Camera(TensorWrapper):
             raise NotImplementedError(model)
 
         data = np.r_[camera['width'], camera['height'],
-                     fx, fy, cx-0.5, cy-0.5, params]
+                     fx, fy, cx - 0.5, cy - 0.5, params]
         return cls(data)
 
     @property
@@ -319,9 +321,9 @@ class Camera(TensorWrapper):
         x, y, z = p3d[..., 0], p3d[..., 1], p3d[..., 2]
         zero = torch.zeros_like(z)
         J = torch.stack([
-            1/z, zero, -x / z**2,
-            zero, 1/z, -y / z**2], dim=-1)
-        J = J.reshape(p3d.shape[:-1]+(2, 3))
+            1 / z, zero, -x / z ** 2,
+            zero, 1 / z, -y / z ** 2], dim=-1)
+        J = J.reshape(p3d.shape[:-1] + (2, 3))
         return J  # N x 2 x 3
 
     @autocast
@@ -362,3 +364,124 @@ class Camera(TensorWrapper):
 
     def __repr__(self):
         return f'Camera {self.shape} {self.dtype} {self.device}'
+
+
+class OusterLidarToBeamTransformer(object):
+    def __init__(self):
+        self.R = torch.from_numpy(np.array([[-1., 0., 0.], [0., -1., 0.], [0., 0., 1.]])).float()
+        self.t = torch.from_numpy(np.array([0., 0., 0.03618])).float()
+        self.zero_pix_offset_azimuth = -10.97 * np.pi / 180.0
+        self.lidar_origin_to_beam_origin_m = 0.02767
+
+    def calculate_encoder_angle(self, p3d_beam_frame):
+        return torch.fmod(torch.atan2(p3d_beam_frame[..., 1], p3d_beam_frame[..., 0]) + 2.0 * torch.pi +
+                          self.zero_pix_offset_azimuth, 2.0 * np.pi).unsqueeze(-1)
+
+    def transform_to_emitter_frame(self, p3d: torch.Tensor):
+        p3d_beam_frame = (p3d - self.t).matmul(self.R.T)
+        encoder = self.calculate_encoder_angle(p3d_beam_frame)
+        offset_correction = torch.concat(
+            (np.cos(encoder), np.sin(encoder), torch.zeros_like(encoder)), dim=-1) * self.lidar_origin_to_beam_origin_m
+        spherical_points_tensor = p3d_beam_frame - offset_correction
+        return spherical_points_tensor, encoder
+
+
+def transform_to_emitter_frame(p3d: torch.Tensor):
+    return OusterLidarToBeamTransformer().transform_to_emitter_frame(p3d)
+
+
+class OusterData(object):
+    def __init__(self):
+        OUSTER_FRAME_PATH = "/home/fu/Desktop/sample_ouster_frame_data"
+        RANGE_IMAGE_PATH = os.path.join(OUSTER_FRAME_PATH, "latest_range.tif")
+        X_IMAGE_PATH = os.path.join(OUSTER_FRAME_PATH, "latest_x.tif")
+        Y_IMAGE_PATH = os.path.join(OUSTER_FRAME_PATH, "latest_y.tif")
+        Z_IMAGE_PATH = os.path.join(OUSTER_FRAME_PATH, "latest_z.tif")
+        self.depth_image = cv2.imread(RANGE_IMAGE_PATH, cv2.IMREAD_ANYDEPTH)
+        self.x_image = cv2.imread(X_IMAGE_PATH, cv2.IMREAD_ANYDEPTH)
+        self.y_image = cv2.imread(Y_IMAGE_PATH, cv2.IMREAD_ANYDEPTH)
+        self.z_image = cv2.imread(Z_IMAGE_PATH, cv2.IMREAD_ANYDEPTH)
+
+
+class OusterLidar(Camera):
+    # eps = 1e-4
+
+    def __init__(self, data: torch.Tensor):
+        assert data.shape[-1] == 8
+        super().__init__(data)
+
+    @property
+    def size(self) -> torch.Tensor:
+        return self._data[..., :2]
+
+    @property
+    def top_left(self) -> torch.Tensor:
+        return self._data[..., 2:4]
+
+    @property
+    def n_azimuth_beams(self):
+        return self._data[..., 4:5]
+
+    @property
+    def n_altitude_beams(self):
+        return self._data[..., 5:6]
+
+    @property
+    def first_altitude_angle(self):
+        return self._data[..., 6:7]
+
+    @property
+    def last_altitude_angle(self):
+        return self._data[..., 7:8]
+
+    def is_in_image(self, p2d: torch.Tensor) -> torch.Tensor:
+        '''Check if 2D points are within the image boundaries.'''
+        assert p2d.shape[-1] == 2
+        size = self.size.unsqueeze(-2)
+        valid = torch.all((p2d >= 0) & (p2d <= (size - 1)), -1)
+        return valid
+
+    @autocast
+    def scale(self, scales: Union[float, int, Tuple[Union[float, int]]]):
+        '''Update the ouster parameters after resizing an image.'''
+        if isinstance(scales, (int, float)):
+            scales = (scales, scales)
+        s = self._data.new_tensor(scales)
+        data = torch.cat([self.size*s, self.top_left*s, self.n_azimuth_beams*s[0], self.n_altitude_beams*s[1],
+                          self.first_altitude_angle, self.last_altitude_angle])
+        return self.__class__(data)
+
+    @autocast
+    def crop(self, left_top: Tuple[float], size: Tuple[int]):
+        '''Update the camera parameters after cropping an image.'''
+        left_top = self._data.new_tensor(left_top)
+        size = self._data.new_tensor(size)
+        data = torch.cat([size, left_top, self.n_azimuth_beams, self.n_altitude_beams,
+                          self.first_altitude_angle, self.last_altitude_angle])
+
+        return self.__class__(data)
+
+    @autocast
+    def world2image(self, p3d: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        '''Transform 3D points into 2D pixel coordinates.'''
+        ranges = torch.linalg.vector_norm(p3d, dim=-1)
+        has_range = ranges > self.eps
+
+        spherical_points_tensor, encoder = transform_to_emitter_frame(p3d)
+
+        spherical_ranges = torch.linalg.vector_norm(spherical_points_tensor, dim=-1)
+        unit_vectors = spherical_points_tensor / spherical_ranges.reshape(*spherical_points_tensor.shape[:-1], 1)
+        altitudes = torch.asin(unit_vectors[..., 2]).unsqueeze(-1)
+        is_in_lidar = (max(self.first_altitude_angle,
+                           self.last_altitude_angle) > altitudes) & (altitudes > min(self.first_altitude_angle,
+                                                                                     self.last_altitude_angle))
+        # TODO: Where is the centre of the pixel? mid-pixel or top-left corner?
+        u = (0.0 * (self.last_altitude_angle - altitudes) +
+             self.n_altitude_beams * (altitudes - self.first_altitude_angle)) / \
+            (self.last_altitude_angle - self.first_altitude_angle)
+
+        v = (2.0 * torch.pi - encoder) / (2 * torch.pi / self.n_azimuth_beams)
+        coords = torch.concat((v, u), dim=-1)
+        coords -= self.top_left
+        valid = has_range & is_in_lidar.squeeze(-1) & self.is_in_image(coords)
+        return coords, valid
