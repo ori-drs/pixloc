24812ad298b65b40e0b8a39d85b9305174cf8411
Frank Fu <fulkast@gmail.com>
2022-01-20 18:41:45 +0000

    Fix halo dataset to be training compatible

--- pixloc/pixlib/datasets/halo.py
+++ pixloc/pixlib/datasets/halo.py
@@ -23,11 +23,39 @@ logger = logging.getLogger(__name__)
 
 class Halo(BaseDataset):
 
+    default_conf = {
+        'dataset_dir': 'halo/',
+        'info_dir': 'halo_trainin/',
+
+        'train_slices': [None],
+        'val_slices': [None],
+        'train_num_per_slice': 1000,
+        'val_num_per_slice': 80,
+
+        'two_view': True,
+        'min_overlap': 0.3,
+        'max_overlap': 1.,
+        'min_baseline': None,
+        'max_baseline': None,
+        'sort_by_overlap': False,
+
+        'grayscale': False,
+        'resize': None,
+        'resize_by': 'max',
+        'crop': None,
+        'pad': None,
+        'optimal_crop': True,
+        'seed': 0,
+
+        'max_num_points3D': 512,
+        'force_num_points3D': False,
+    }
+
     def _init(self, conf):
         pass
 
     def get_dataset(self, split):
-        return _Halo_Dataset(self.conf)
+        return _Halo_Dataset(self.conf, split)
 
 
 def drs_q_t_to_T(q, t):
@@ -42,7 +70,9 @@ def drs_q_t_to_T(q, t):
 
 
 class _Halo_Dataset(torch.utils.data.Dataset):
-    def __init__(self, conf):
+    def __init__(self, conf, split):
+        self.conf = conf
+        self.split = split
         self.BAG_PATH = "/home/fu/Desktop/sample_ouster_frame_data/halo_data.bag"
         self.bridge = CvBridge()
         with rosbag.Bag(self.BAG_PATH) as bag:
@@ -55,9 +85,9 @@ class _Halo_Dataset(torch.utils.data.Dataset):
         with open(CONFIG_PATH, "r") as f:
             metadata = client.SensorInfo(f.read())
 
-        W = 500  # metadata.format.columns_per_frame
+        W = 250  # metadata.format.columns_per_frame
         H = metadata.format.pixels_per_column
-        left_x = 350
+        left_x = 450
         top_y = 0
         n_azimuth_beams = metadata.format.columns_per_frame
         n_altitude_beams = metadata.format.pixels_per_column
@@ -97,7 +127,8 @@ class _Halo_Dataset(torch.utils.data.Dataset):
                                  interpolation=cv2.INTER_LINEAR,
                                  borderMode=cv2.BORDER_CONSTANT)
         camera_image = (camera_image.astype(np.float32) / 255. - 0.5)[:, :, None]
-        # camera_image = np.tile(camera_image, (1, 1, 3))
+        zeros = np.zeros_like(camera_image)
+        camera_image = np.dstack((camera_image, zeros, zeros))
         near_ir_image = self.bridge.imgmsg_to_cv2(self.data.lidar_nearir_image, "mono16").astype(np.float32)
         near_ir_image = near_ir_image[:, int(self.ouster_sensor.top_left[0].item()):
                                          int(self.ouster_sensor.top_left[0] + self.ouster_sensor.size[0])]
@@ -105,7 +136,8 @@ class _Halo_Dataset(torch.utils.data.Dataset):
         ir_span = max(near_ir_image.max(), -near_ir_image.min())
         near_ir_image /= ir_span
         near_ir_image = near_ir_image[:, :, None]
-        # near_ir_image = np.tile(near_ir_image[:, :, None], (1, 1, 3))
+        zeros = np.zeros_like(near_ir_image)
+        near_ir_image = np.dstack((zeros, near_ir_image, zeros))
         points = [point[:3] for point in point_cloud2.read_points(self.data.lidar_points, skip_nans=True)]
         points = np.array(points).reshape(int(self.ouster_sensor.n_altitude_beams),
                                           int(self.ouster_sensor.n_azimuth_beams), 3)
@@ -129,7 +161,7 @@ class _Halo_Dataset(torch.utils.data.Dataset):
 
         datum['T_r2q_gt'] = Pose.from_4x4mat(torch.from_numpy(self.T_left_camera_lidar))
         pose_error = np.array([[1,0,0,0],
-                               [0,1,0,0.1],
+                               [0,1,0,0.0],
                                [0,0,1,0],
                                [0,0,0,1]]).astype(np.float32)
 
@@ -144,3 +176,6 @@ class _Halo_Dataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return 1
+
+    def no_op(self, seed):
+        pass
